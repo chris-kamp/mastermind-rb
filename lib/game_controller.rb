159986aaf0e9e9191be3fb9edf35e08994516f38
Chris Kamp <chriskamp101@gmail.com>
2021-01-26 12:58:50 +1000

    Make winner display more prominent

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -39,10 +39,16 @@ class GameController
   def post_turn(hint)
     if (breaker_won?(hint))
       @game_over = true
-      @display.print_colorised_text("Breaker wins\n", :green)
+      @display.print_colorised_text(
+        "\n------------------\nBREAKER WINS!\n------------------\n\n",
+        :green,
+      )
     elsif (out_of_turns?)
       @game_over = true
-      @display.print_colorised_text("Maker wins\n", :green)
+      @display.print_colorised_text(
+        "\n------------------\nMAKER WINS!\n------------------\n\n",
+        :green,
+      )
     else
       @display.print_colorised_text("#{@turns} guesses remaining\n", :red)
     end
@@ -50,13 +56,16 @@ class GameController
 
   # Ask player whether to play again
   def quit?
-    @display.print_colorised_text("Do you want to play again? Y / N\n", :green)
+    @display.print_colorised_text(
+      "Do you want to play again? (Y / N)\n",
+      :green,
+    )
     yes = 'y'
     no = 'n'
     answer = gets.chomp.downcase
     until (answer == yes || answer == no)
       @display.print_colorised_text(
-        "I didn't quite catch that. Do you want to play again? Y / N\n",
+        "I didn't quite catch that. Do you want to play again? (Y / N)\n",
         :green,
       )
       answer = gets.chomp.downcase
